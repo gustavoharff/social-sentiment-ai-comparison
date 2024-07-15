00819804f2b1430c1153f73e6cfa81cbfdb67808
Gustavo Harff <gustavo.harff@gmail.com>
2024-07-15 20:31:08 -0300

    update

--- apps/web/src/app/pipelines/[id]/report/page.tsx
+++ apps/web/src/app/pipelines/[id]/report/page.tsx
@@ -50,7 +50,13 @@ type Data = {
 type Data2 = {
   date: string
   comments: number
-  sentiment: 'positive' | 'negative' | 'neutral'
+  sentiment: 'positive' | 'negative' | 'neutral' | 'mixed'
+}
+
+interface Item {
+  date: string
+  sentiment: Data2['sentiment']
+  comments: number
 }
 
 export default function Report() {
@@ -85,9 +91,13 @@ export default function Report() {
           acc.neutral += 1
         }
 
+        if (comment.sentiment === 'mixed') {
+          acc.mixed += 1
+        }
+
         return acc
       },
-      { positive: 0, negative: 0, neutral: 0 },
+      { positive: 0, negative: 0, neutral: 0, mixed: 0 },
     )
 
     const total = counts.positive + counts.negative + counts.neutral
@@ -95,6 +105,7 @@ export default function Report() {
     const positivePercentage = (counts.positive / total) * 100
     const negativePercentage = (counts.negative / total) * 100
     const neutralPercentage = (counts.neutral / total) * 100
+    const mixedPercentage = (counts.mixed / total) * 100
 
     return [
       {
@@ -112,21 +123,15 @@ export default function Report() {
         name: 'Neutro',
         value: Number.isNaN(neutralPercentage) ? 0 : neutralPercentage,
       },
+      {
+        id: 'mixed',
+        name: 'Misto',
+        value: Number.isNaN(mixedPercentage) ? 0 : mixedPercentage,
+      },
     ]
   }, [comments])
 
   const perDayData = useMemo(() => {
-    const positiveComments = comments.filter((c) => c.sentiment === 'positive')
-    const negativeComments = comments.filter((c) => c.sentiment === 'negative')
-    const neutralComments = comments.filter((c) => c.sentiment === 'neutral')
-    const mixedComments = comments.filter((c) => c.sentiment === 'mixed')
-
-    interface Item {
-      date: string
-      sentiment: (typeof comments)[0]['sentiment']
-      comments: number
-    }
-
     const positiveItems: Item[] = []
     const negativeItems: Item[] = []
     const neutralItems: Item[] = []
@@ -209,176 +214,298 @@ export default function Report() {
     return [...positiveItems, ...negativeItems, ...neutralItems, ...mixedItems]
   }, [comments])
 
+  // const fake: Item[] = [
+  //   {
+  //     date: '2024-06-01',
+  //     sentiment: 'positive',
+  //     comments: 6,
+  //   },
+  //   {
+  //     date: '2024-06-01',
+  //     sentiment: 'negative',
+  //     comments: 3,
+  //   },
+  //   {
+  //     date: '2024-06-01',
+  //     sentiment: 'neutral',
+  //     comments: 2,
+  //   },
+  //   {
+  //     date: '2024-06-02',
+  //     sentiment: 'positive',
+  //     comments: 10,
+  //   },
+  //   {
+  //     date: '2024-06-02',
+  //     sentiment: 'negative',
+  //     comments: 4,
+  //   },
+  //   {
+  //     date: '2024-06-02',
+  //     sentiment: 'neutral',
+  //     comments: 6,
+  //   },
+  //   {
+  //     date: '2024-06-03',
+  //     sentiment: 'positive',
+  //     comments: 12,
+  //   },
+  //   {
+  //     date: '2024-06-03',
+  //     sentiment: 'negative',
+  //     comments: 6,
+  //   },
+  //   {
+  //     date: '2024-06-03',
+  //     sentiment: 'neutral',
+  //     comments: 6,
+  //   },
+  //   {
+  //     date: '2024-06-04',
+  //     sentiment: 'positive',
+  //     comments: 19,
+  //   },
+  //   {
+  //     date: '2024-06-04',
+  //     sentiment: 'negative',
+  //     comments: 7,
+  //   },
+  //   {
+  //     date: '2024-06-04',
+  //     sentiment: 'neutral',
+  //     comments: 9,
+  //   },
+  //   {
+  //     date: '2024-06-05',
+  //     sentiment: 'positive',
+  //     comments: 24,
+  //   },
+  //   {
+  //     date: '2024-06-05',
+  //     sentiment: 'negative',
+  //     comments: 7,
+  //   },
+  //   {
+  //     date: '2024-06-05',
+  //     sentiment: 'neutral',
+  //     comments: 11,
+  //   },
+  //   {
+  //     date: '2024-06-06',
+  //     sentiment: 'positive',
+  //     comments: 34,
+  //   },
+  //   {
+  //     date: '2024-06-06',
+  //     sentiment: 'negative',
+  //     comments: 8,
+  //   },
+  //   {
+  //     date: '2024-06-06',
+  //     sentiment: 'neutral',
+  //     comments: 15,
+  //   },
+  // ]
+
   return (
     <Spin spinning={loading}>
       <div className="mx-auto mt-4 flex w-[calc(100%-4rem)] flex-col gap-4">
         <h1 className="text-2xl font-bold">Relatório</h1>
 
         <div className="flex justify-between">
-          <Pie
-            className="max-h-96 w-1/2"
-            animate={false}
-            angleField="value"
-            colorField="color"
-            legend={false}
-            tooltip={false}
-            data={data}
-            innerRadius={0.6}
-            style={{
-              lineWidth: 3,
-              stroke: (data: Data) => {
-                const tone = resolvedTheme === 'dark' ? 800 : 200
+          <div className="flex w-1/2 flex-col items-center gap-4">
+            <h4 className="text-xl font-semibold">Sentimentos</h4>
+            <Pie
+              className="max-h-96 w-1/2"
+              animate={false}
+              angleField="value"
+              colorField="color"
+              legend={false}
+              tooltip={false}
+              data={data}
+              innerRadius={0.6}
+              style={{
+                lineWidth: 3,
+                stroke: (data: Data) => {
+                  const tone = resolvedTheme === 'dark' ? 800 : 200
 
-                if (data.id === 'positive') {
-                  return colors.green[tone]
-                }
+                  if (data.id === 'positive') {
+                    return colors.green[tone]
+                  }
 
-                if (data.id === 'negative') {
-                  return colors.red[tone]
-                }
+                  if (data.id === 'negative') {
+                    return colors.red[tone]
+                  }
 
-                if (data.id === 'neutral') {
-                  return colors.gray[tone]
-                }
-              },
-              inset: 2,
-              radius: 10,
-              fill: (data: Data) => {
-                const tone = resolvedTheme === 'dark' ? 400 : 600
+                  if (data.id === 'neutral') {
+                    return colors.gray[tone]
+                  }
+                },
+                inset: 2,
+                radius: 10,
+                fill: (data: Data) => {
+                  const tone = resolvedTheme === 'dark' ? 400 : 600
 
-                if (data.id === 'positive') {
-                  return colors.green[tone]
-                }
+                  if (data.id === 'positive') {
+                    return colors.green[tone]
+                  }
 
-                if (data.id === 'negative') {
-                  return colors.red[tone]
-                }
+                  if (data.id === 'negative') {
+                    return colors.red[tone]
+                  }
 
-                if (data.id === 'neutral') {
-                  return colors.gray[tone]
-                }
-              },
-            }}
-            label={{
-              text: (data: Data) => {
-                const percentage = data.value.toFixed(0) + '%'
+                  if (data.id === 'neutral') {
+                    return colors.gray[tone]
+                  }
+                },
+              }}
+              label={{
+                text: (data: Data) => {
+                  const percentage = data.value.toFixed(0) + '%'
 
-                if (percentage === '0%') {
-                  return ''
-                }
+                  if (percentage === '0%') {
+                    return ''
+                  }
 
-                return data.name + '\n' + percentage
-              },
-              style: {
-                fontSize: 14,
-                fontWeight: '600',
-              },
-              fill: (data: Data) => {
-                const tone = resolvedTheme === 'dark' ? 800 : 200
-
-                if (data.id === 'positive') {
-                  return colors.green[tone]
-                }
-
-                if (data.id === 'negative') {
-                  return colors.red[tone]
-                }
-
-                if (data.id === 'neutral') {
-                  return colors.gray[tone]
-                }
-              },
-            }}
-            annotations={[
-              {
-                type: 'text',
-                tooltip: false,
+                  return data.name + '\n' + percentage
+                },
                 style: {
-                  text: comments.length + '\ncomentários',
-                  x: '50%',
-                  y: '50%',
-                  textAlign: 'center',
-                  fontStyle: 'bold',
-                  fontSize: 24,
-                  fill: resolvedTheme === 'dark' ? 'white' : 'black',
+                  fontSize: 14,
+                  fontWeight: '600',
                 },
-              },
-            ]}
-          />
+                fill: (data: Data) => {
+                  const tone = resolvedTheme === 'dark' ? 800 : 200
 
-          <Line
-            className="w-1/2"
-            theme={resolvedTheme === 'dark' ? 'custom-dark' : 'light'}
-            data={perDayData}
-            meta={{
-              comments: {
-                alias: 'Comentários',
-              },
-            }}
-            padding="auto"
-            xField="date"
-            yField="comments"
-            seriesField="sentiment"
-            tooltip={{
-              title: (title) => {
-                console.log(title)
-                return dayjs(title).format('DD/MM/YYYY')
-              },
-              formatter: (data) => {
-                return {
-                  name: 'Comentários',
-                  value: data.comments,
-                }
-              },
-            }}
-            xAxis={{
-              label: {
-                formatter: (v) => {
-                  return dayjs(v).format('DD/MM')
+                  if (data.id === 'positive') {
+                    return colors.green[tone]
+                  }
+
+                  if (data.id === 'negative') {
+                    return colors.red[tone]
+                  }
+
+                  if (data.id === 'neutral') {
+                    return colors.gray[tone]
+                  }
                 },
-              },
-              grid: {
-                line: {
+              }}
+              annotations={[
+                {
+                  type: 'text',
+                  tooltip: false,
                   style: {
-                    stroke:
-                      resolvedTheme === 'dark'
-                        ? colors.zinc[800]
-                        : colors.zinc[200],
+                    text: comments.length + '\ncomentários',
+                    x: '50%',
+                    y: '50%',
+                    textAlign: 'center',
+                    fontStyle: 'bold',
+                    fontSize: 24,
+                    fill: resolvedTheme === 'dark' ? 'white' : 'black',
                   },
                 },
-                alternateColor: 'rgba(0,0,0,0.05)',
-              },
-            }}
-            color={(datum) => {
-              const color = {
-                positive: colors.green[500],
-                negative: colors.red[500],
-                neutral: colors.gray[500],
-              }
+              ]}
+            />
+          </div>
 
-              return color[datum.sentiment as unknown as Data2['sentiment']]
-            }}
-            point={{
-              size: 4,
-              color: colors.green[500],
-              shape: 'circle',
-              style: ({ sentiment }: Data2) => {
+          <div className="flex w-1/2 flex-col items-center  gap-4">
+            <h4 className="text-xl font-semibold">Comentários por dia</h4>
+
+            <Line
+              className="w-1/2"
+              theme={resolvedTheme === 'dark' ? 'custom-dark' : 'light'}
+              data={perDayData}
+              meta={{
+                comments: {
+                  alias: 'Comentários',
+                },
+              }}
+              padding="auto"
+              xField="date"
+              yField="comments"
+              seriesField="sentiment"
+              legend={{
+                itemName: {
+                  formatter: (text) => {
+                    if (text === 'positive') {
+                      return 'Positivo'
+                    }
+
+                    if (text === 'negative') {
+                      return 'Negativo'
+                    }
+
+                    if (text === 'neutral') {
+                      return 'Neutro'
+                    }
+
+                    if (text === 'mixed') {
+                      return 'Misto'
+                    }
+                  },
+                },
+              }}
+              tooltip={{
+                title: (title) => {
+                  return dayjs(title).format('DD/MM/YYYY')
+                },
+                formatter: (data) => {
+                  return {
+                    name: 'Comentários',
+                    value: data.comments,
+                  }
+                },
+              }}
+              xAxis={{
+                label: {
+                  formatter: (v) => {
+                    return dayjs(v).format('DD/MM')
+                  },
+                },
+                grid: {
+                  line: {
+                    style: {
+                      stroke:
+                        resolvedTheme === 'dark'
+                          ? colors.zinc[800]
+                          : colors.zinc[200],
+                    },
+                  },
+                  alternateColor: 'rgba(0,0,0,0.05)',
+                },
+              }}
+              color={(datum) => {
                 const color = {
                   positive: colors.green[500],
                   negative: colors.red[500],
                   neutral: colors.gray[500],
+                  mixed: colors.yellow[500],
                 }
 
-                return {
-                  fill: color[sentiment],
-                  stroke: color[sentiment],
-                  lineWidth: 1,
-                }
-              },
-            }}
-            annotations={[]}
-            smooth
-          />
+                return color[datum.sentiment as unknown as Data2['sentiment']]
+              }}
+              point={{
+                size: 4,
+                color: colors.green[500],
+                shape: 'circle',
+                style: ({ sentiment }: Data2) => {
+                  const color = {
+                    positive: colors.green[500],
+                    negative: colors.red[500],
+                    neutral: colors.gray[500],
+                    mixed: colors.yellow[500],
+                  }
+
+                  return {
+                    fill: color[sentiment],
+                    stroke: color[sentiment],
+                    lineWidth: 1,
+                  }
+                },
+              }}
+              annotations={[]}
+              smooth
+            />
+          </div>
         </div>
 
         <Table
